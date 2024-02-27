63132684e8068dfeace0731c0550f32c505f8f82
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2024-02-27 20:45:50 +0700

    test git for vs

---
+++ data/data.cs
@@ -0,0 +1,12 @@
+// namespace QLTTTM.Datas {
+//     // Singleton
+//     public class DbContext {
+//         private static readonly DataSQLContext instance = new DataSQLContext();
+//         public static DbContext Instance {
+//             get {
+//                 return instance;
+//             }
+//         }
+//         private DbContext() {}
+//     }
+// }
